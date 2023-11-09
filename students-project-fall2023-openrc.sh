21f893bee15cdd4705a80f20eb4f2532d0e6d037
Riccardo11111 <61868130+Riccardo11111@users.noreply.github.com>
2023-11-09 18:13:46 +0100

    Update students-project-fall2023-openrc.sh

--- students-project-fall2023-openrc.sh
+++ students-project-fall2023-openrc.sh
@@ -3,7 +3,7 @@ export OS_AUTH_URL=https://testbed.expeca.proj.kth.se:5000/v3
 export OS_IDENTITY_API_VERSION=3
 export OS_INTERFACE=public
 export OS_PROJECT_ID="ddf790cc1bb64e0387eff22c1ab19f94"
-export OS_USERNAME="ata"
+export OS_USERNAME="gourav"
 export OS_USER_DOMAIN_NAME="Default"
 if [ -z "$OS_USER_DOMAIN_NAME" ]; then unset OS_USER_DOMAIN_NAME; fi
 export OS_AUTH_TYPE="password"
